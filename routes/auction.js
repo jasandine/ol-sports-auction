a28b1fd07684f52bc5b573df991c770f5a6af9b6
David Zulaica and Joey Sandine <zulaica@gmail.com>
2015-03-04 14:32:40 -0800

    Create static auction object.

--- routes/auction.js
+++ routes/auction.js
@@ -1,5 +1,14 @@
 Olsport.AuctionRoute = Ember.Route.extend({
+  beforeModel: function() {
+    this.store.push('auction', {
+      id: 1,
+      name: "(19)20's Merch",
+      location: "Manhattan",
+      date: "March 14th, 2015",
+      description: "All Gatsby up in here, y'all!"
+    });
+  },
   model: function() {
-    return this.store.find('lot');
+    return this.store.find('auction');
   }
 });
